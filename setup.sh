6b12234c5d72638755cc5bf7ea5e65e4a1a9e0c6
Shinwoo PARK <shinwoo.park@psw.kr>
2023-11-20 22:00:22 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update -y && sudo apt upgrade -y
 
-curl -sS https://starship.rs/install.sh | sh
+curl -sS https://starship.rs/install.sh | sh --yes
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 echo "
 plugins=(git)
