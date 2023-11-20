1f078689f29f4cdf777e44c398771a0f5e592994
Shinwoo PARK <shinwoo.park@psw.kr>
2023-11-20 22:39:10 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "
+[settings]
+api_key = waka_2dc69c6d-3ab3-4735-abc8-f2451ac533f0" > $HOME/.wakatime.cfg
+
 sudo apt update -y && sudo apt upgrade -y
 
 curl -sS --out $HOME/starship.sh https://starship.rs/install.sh && sh $HOME/starship.sh -y
@@ -9,4 +13,5 @@ DISABLE_AUTO_UPDATE=true
 DISABLE_UPDATE_PROMPT=true
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 eval \"$(starship init zsh)\"" > $HOME/.zshrc
+
 cp ./.config/starship.toml $HOME/.config/starship.toml
